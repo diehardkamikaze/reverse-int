b5a24aa5ea1a7791d3216ca0a13a45e0cee0e025
diehardkamikaze <diehardkamikaze@gmail.com>
2021-02-27 20:41:03 +0300

    init: implementation reverse-int completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  var result;
   
+  if (n < 0)
+	  n *=-1;
+  result = 0; 
+  while (n)
+  {
+	  result = result * 10 + n % 10;
+	  n = Math.floor(n / 10);
+  }
+  return (result);
 }
